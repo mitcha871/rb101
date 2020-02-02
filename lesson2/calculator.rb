c0ca9238a3d2982a35e68a9cc7eeb7519c3ae64d
mitcha871 <mitchaoki@gmail.com>
2020-02-02 23:18:06 +0900

    calc

--- lesson2/calculator.rb
+++ lesson2/calculator.rb
@@ -1,6 +1,19 @@
 puts "Give me a number"
-number1 = gets.chomp.to_i
+number1 = gets.chomp
 puts "Give me another number"
-number2 = gets.chomp.to_i
+number2 = gets.chomp
 
-puts number1 + number2
+puts "Select an operation: 1) add 2) subtract 3) multiply 4) divide"
+operation = gets.chomp.to_i
+
+if operation == 1
+  result = number1.to_i + number2.to_i
+elsif operation == 2
+  result = number1.to_i - number2.to_i
+elsif operation == 3
+  result = number1.to_i * number2.to_i
+elsif operation == 4
+  result = number1.to_f / number2.to_f
+end
+
+puts "Your number is #{result}"
