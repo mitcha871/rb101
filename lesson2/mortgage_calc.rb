bc4603beb16af692d40df5199ec6a22ca0bbae1b
mitcha871 <mitchaoki@gmail.com>
2020-02-06 23:25:53 +0900

    rock paper scissors

--- lesson2/mortgage_calc.rb
+++ lesson2/mortgage_calc.rb
@@ -1,14 +1,21 @@
 # Welcome the user
 # Ask for: the loan amount (float); the APR (float); the loan duration (int)
-# use the above information to return the monthly interest rate (%float); the loan duration (int)
-
+# return the monthly interest rate (%float); the loan duration (int)
 
 def message(msg)
-  puts msg = "=> #{msg}"
+  puts "=> #{msg}"
 end
 
-def valid_float?(loan_amount)
-  loan_amount.to_i > 0 && loan_amount.to_f.to_s == Float(loan_amount).to_s
+def valid_number?(input)
+  integer?(input) || float?(input)
+end
+
+def integer?(input)
+  Integer(input) rescue false
+end
+
+def float?(input)
+  Float(input) rescue false
 end
 
 message("This program helps you calculate your loan information")
@@ -18,23 +25,42 @@ loan_amount = ''
 
 loop do
   loan_amount = gets.chomp
-  if loan_amount = valid_float?(loan_amount)
+  if valid_number?(loan_amount) && loan_amount.to_f > 0
+    loan_amount = loan_amount.to_f
     break
   else
     message("Oops! Please enter a valid loan amount!")
   end
 end
 
-message("What is the APR?")
+message("What is the APR? e.g. 5.4 for 5.4%")
 apr = ''
+monthly_rate = ''
 
 loop do
   apr = gets.chomp
-  if apr = valid_float?(apr)
+  if valid_number?(apr) && apr.to_f >= 0
+    monthly_rate = apr.to_f / 12 / 100
     break
   else
     message("Oops! Please enter a valid APR!")
   end
 end
 
+message("What is the loan duration (in years)?")
+loan_duration = ''
 
+loop do
+  loan_duration = gets.chomp
+  if valid_number?(loan_duration) && loan_duration.to_f > 0
+    loan_duration = loan_duration.to_f * 12
+    break
+  else
+    message("Oops! Please enter a valid loan_duration!")
+  end
+end
+
+monthly_payment = loan_amount * (monthly_rate / 
+                  (1 - (1 + monthly_rate)**(-loan_duration)))
+
+message("Your monthly payment is $#{monthly_payment.round(2)}")
---
+++ lesson2/rock_paper_scissors_v2.rb
@@ -0,0 +1,117 @@
+VALID_CHOICES = %w(rock paper scissors lizard Spock)
+
+def prompt(message)
+  puts("=> #{message}")
+end
+
+def map(input)
+  case input
+  when 'r'
+    'rock'
+  when 'p'
+    'paper'
+  when 's'
+    'scissors'
+  when 'l'
+    'lizard'
+  when 'S'
+    'Spock'
+  end
+end
+
+def win?(first, second)
+  case first
+  when 'rock'
+    if ['scissors', 'lizard'].include?(second)
+      true
+    end
+
+  when 'scissors'
+    if ['paper', 'lizard'].include?(second)
+      true
+    end
+
+  when 'paper'
+    if ['rock', 'Spock'].include?(second)
+      true
+    end
+
+  when 'lizard'
+    if ['Spock', 'paper'].include?(second)
+      true
+    end
+
+  when 'Spock'
+    if ['scissors', 'rock'].include?(second)
+      true
+    end
+  end
+
+  false
+end
+
+def return_winner(player, computer)
+  if win?(player, computer)
+    'player'
+  elsif win?(computer, player)
+    'computer'
+  else
+    'draw'
+  end
+end
+
+def declare_winner(winner)
+  prompt("The match is over!")
+  prompt("The grand winner is... #{winner}!")
+end
+
+player_score = 0
+computer_score = 0
+
+loop do
+  loop do
+    choice = ''
+    loop do
+      prompt("Choose one: #{VALID_CHOICES.join(', ')}")
+      choice = gets.chomp
+
+      if VALID_CHOICES.include?(choice)
+        break
+      else
+        choice = map(choice)
+        break if VALID_CHOICES.include?(choice)
+        prompt('That\'s not a valid choice!')
+      end
+    end
+
+    computer_choice = VALID_CHOICES.sample
+
+    prompt("You chose #{choice}; Computer chose #{computer_choice}")
+
+    if return_winner(choice, computer_choice) == 'player'
+      prompt 'You won!'
+      player_score += 1
+    elsif return_winner(choice, computer_choice) == 'computer'
+      prompt 'You lost!'
+      computer_score += 1
+    else
+      prompt 'It\'s a tie!'
+    end
+
+    prompt("You: #{player_score}. Computer: #{computer_score}")
+
+    break if player_score == 5 || computer_score == 5
+  end
+
+  declare_winner player_score == 5 ? 'You' : 'The computer'
+
+  prompt("The final score was You: #{player_score}; Computer #{computer_score}")
+
+  prompt('Do you want to play again?')
+  answer = gets.chomp
+  break unless answer.downcase.start_with?('y')
+  player_score = 0
+  computer_score = 0
+end
+
+prompt('Thanks for playing! Goodbye!')
